4121be0886c1bb588950723023c336e23216f2ec
Mariia <mariia.alokhina@gmail.com>
2019-01-03 19:17:10 +0200

    1

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,14 @@ Rails.application.routes.draw do
   root to: "questions#index"
 
   mount ActionCable.server => '/cable'
+
+  namespace :api do
+    namespace :v1 do
+      resource :profiles do
+        get :me, on: :collection
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
